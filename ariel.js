e59b562c9e07c2c05aa8b746090d88d8bf4b0ffa
Tyler Hernandez <tylerhernandez@Tylers-MacBook-Pro.local>
2019-08-13 13:37:32 -0400

    push

--- ariel.js
+++ ariel.js
@@ -10,10 +10,10 @@ function cardPicker() {
   console.log(cardForPlay)
 }
 
-Joke1 = Card("Joker", 1)
-One1 = Card("One", 1)
-Joke2 = Card("Joker", 2)
-One2 = Card("One", 2)
+Joke1 = new Card("Joker", 1)
+One1 = new Card("One", 1)
+Joke2 = new Card("Joker", 2)
+One2 = new Card("One", 2)
 
 let cards = [Joke1, One1, Joke2, One2];
 let cardNums = [0,1,2,3];
