1a3ffcb6a18fa241a705da14aed6e5172322542e
Ariel <zzzderpzzz@gmail.com>
2019-08-13 13:44:07 -0400

    first test of card generator

--- ariel.js
+++ ariel.js
@@ -7,13 +7,20 @@ class Card{
 
 function cardPicker() {
   let cardForPlay = Math.floor(Math.random()*cardNums.length)
-  console.log(cardForPlay)
+  console.log(cardForPlay);
+  let cardPicked = cards[cardForPlay];
+  cards.splice(cardForPlay);
+  cardNums.splice(cardForPlay);
+  return cardPicked;
 }
 
-Joke1 = Card("Joker", 1)
-One1 = Card("One", 1)
-Joke2 = Card("Joker", 2)
-One2 = Card("One", 2)
+Joke1 = new Card("Joker", 1)
+One1 = new Card("One", 1)
+Joke2 = new Card("Joker", 2)
+One2 = new Card("One", 2)
 
 let cards = [Joke1, One1, Joke2, One2];
 let cardNums = [0,1,2,3];
+console.log(cardPicker());
+console.log(cards);
+console.log(cardNums);
