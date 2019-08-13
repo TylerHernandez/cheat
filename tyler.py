01f6d19f21a7514722bda4f1821e6bad8f267243
Tyler Hernandez <tylerhernandez@Tylers-MacBook-Pro.local>
2019-08-13 13:45:12 -0400

    m

--- tyler.py
+++ tyler.py
@@ -1,10 +1,19 @@
-carddeck = [["RedAce", "BlackAce"],["RedOne","BlackOne"]];
+import random
+carddeck = [["RedAce", "BlackAce"],["RedOne","BlackOne"]]
 # print carddeck[0][1]
 #     carddeck [0][0]= "RedAce"
 
-usershand= [carddeck[0][1], carddeck [1][0]];
+usershand= [carddeck[0][1], carddeck [1][0]]
 
-print usershand;
+print usershand
 
 
 def shuffle():
+    for x in
+    userhand= [carddeck[randomint]
+
+
+for(int i=0; i<52/(playercount); i++)
+{
+userhand.add(carddeck[randomint][randomint])
+}
